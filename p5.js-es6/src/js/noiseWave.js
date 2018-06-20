e498ca14e6669afc6e652e2a7f83aeac2ea3f81f
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-06-21 00:09:12 +0200

    add new example from processing website

---
+++ p5.js-es6/src/js/noiseWave.js
@@ -0,0 +1,38 @@
+const noiseWave = p => {
+  let canvas;
+  let yoff = 0.0;
+  const width = 640;
+  const height = 360;
+
+  p.preload = () => {
+  };
+
+  p.setup = () => {
+    canvas = p.createCanvas(width, height);
+  };
+
+  p.draw = () => {
+    p.background(51);
+    p.fill(255);
+
+    p.beginShape();
+    let xoff = 0;
+
+    for(let x = 0; x <= width; x += 5) {
+      const y = p.map(p.noise(xoff, yoff), 0, 1, 200, 300);
+
+      p.vertex(x, y);
+      xoff += 0.05;
+    }
+    yoff += 0.01;
+    p.vertex(width, height);
+    p.vertex(0, height);
+    p.endShape(p.CLOSE);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+};
+
+export default noiseWave;
