f02c0a50742f77bd2bff0478a7f65cc73e581c81
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-06-21 00:10:18 +0200

    add noiseWave as default example

--- p5.js-es6/src/js/index.js
+++ p5.js-es6/src/js/index.js
@@ -1,6 +1,6 @@
 import p5 from 'p5';
 import '../css/style.scss';
-import gettingStarted from "./getting-started";
-import moltenBarChart from "./moltenBarChart";
-
-new p5(moltenBarChart);
+import sketch from "./sketch";
+import pointillism from "./pointillism";
+import noiseWave from "./noiseWave";
+new p5(noiseWave);
