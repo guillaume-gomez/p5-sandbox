d2b612354117966099849a8c26e27ce9de004f02
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-06-27 07:10:06 +0200

    use gameoflife as index

--- p5.js-es6/src/js/index.js
+++ p5.js-es6/src/js/index.js
@@ -1,6 +1,11 @@
 import p5 from 'p5';
+import "p5/lib/addons/p5.dom";
+
 import '../css/style.scss';
+
 import sketch from "./sketch";
 import pointillism from "./pointillism";
-import noiseWave from "./noiseWave";
-new p5(noiseWave);
+import pixelize from "./pixelize";
+import gameOfLife from "./gameOfLife";
+
+new p5(gameOfLife);
