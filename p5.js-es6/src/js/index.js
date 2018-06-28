1416ccf7f5d248155c171207beb2abca716d5227
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-06-29 00:36:27 +0200

    clean up index.jse

--- p5.js-es6/src/js/index.js
+++ p5.js-es6/src/js/index.js
@@ -3,10 +3,10 @@ import "p5/lib/addons/p5.dom";
 
 import '../css/style.scss';
 
-import sketch from "./sketch";
+/*import sketch from "./sketch";
 import pointillism from "./pointillism";
 import pixelize from "./pixelize";
 import gameOfLife from "./gameOfLife";
-import spirograph from "./spirograph";
+*/import spirograph from "./spirograph";
 
 new p5(spirograph);
