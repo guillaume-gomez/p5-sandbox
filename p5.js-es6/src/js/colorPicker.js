d1420db73c62793959e22bc8327a8b54cc7d97c2
Guillaume Gomez <guillaume.gomez846@gmail.com>
2018-07-11 16:02:47 +0200

    make color picker in extended class from P5.element

--- p5.js-es6/src/js/colorPicker.js
+++ p5.js-es6/src/js/colorPicker.js
@@ -1,29 +1,36 @@
-const colorPicker = (p) => {
-  let redSlider;
-  let greenSlider;
-  let blueSlider;
+import p5 from 'p5';
+import "p5/lib/addons/p5.dom";
 
-  p.createColorPicker = (x, y, color = "#FFFFFF") => {
-    const red = color.slice(1, 3);
-    redSlider = p.createSlider(0, 255, parseInt(red));
-    redSlider.position(x, y);
-    redSlider.input(p.radiusSliderUpdated);
-
-    const green = color.slice(3, 5);
-    greenSlider = p.createSlider(0, 255, parseInt(green));
-    greenSlider.position(x, y + 30);
-    greenSlider.input(p.radiusSliderUpdated);
-
-    const blue = color.slice(5, 7);
-    blueSlider = p.createSlider(0, 255, parseInt(blue));
-    blueSlider.position(x, y + 60);
-    blueSlider.input(p.radiusSliderUpdated);
+class colorPicker extends p5.Element {
+  constructor(pInst) {
+    var elt = document.createElement('input');
+    super(elt, pInst);
+    this.redSlider;
+    this.greenSlider;
+    this.blueSlider;
   }
 
-  p.getColor = () => {
-    return `#${redSlider.value()}${greenSlider.value()}${blueSlider.value()}`
+  createColorPicker(x, y, color = "#FFFFFF"){
+    // const red = color.slice(1, 3);
+    // redSlider = p.createSlider(0, 255, parseInt(red));
+    // redSlider.position(x, y);
+    // redSlider.input(p.radiusSliderUpdated);
+
+    // const green = color.slice(3, 5);
+    // greenSlider = p.createSlider(0, 255, parseInt(green));
+    // greenSlider.position(x, y + 30);
+    // greenSlider.input(p.radiusSliderUpdated);
+
+    // const blue = color.slice(5, 7);
+    // blueSlider = p.createSlider(0, 255, parseInt(blue));
+    // blueSlider.position(x, y + 60);
+    // blueSlider.input(p.radiusSliderUpdated);
   }
 
+  getColor(){
+    // return `#${redSlider.value()}${greenSlider.value()}${blueSlider.value()}`
+    return "#000000";
+  }
 
 };
 export default colorPicker;
