f6e6e3189a1f10a2afa5660f1a633fe3e0358bd6
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-07-17 00:02:50 +0200

    fix how the algorithm check checkboxes and draw image

--- p5.js-es6/src/js/checkboxMirror.js
+++ p5.js-es6/src/js/checkboxMirror.js
@@ -1,16 +1,19 @@
-import '../css/style.scss';
-
+import '../css/styleCheckboxMirror.scss';
 
 const checkboxMirror = p => {
   let canvas;
   let img;
   let slider;
   const vScale = 4;
-  const width = 600;
-  const height = 300;
+  const width = 200;
+  const height = 200;
+
+  let rows = width / vScale;
+  let cols = height / vScale;
+  let checkboxes = [];
 
   p.preload = () => {
-    img = p.loadImage("assets/worldcup.jpg");
+    img = p.loadImage("assets/sunflower.jpg");
   };
 
   p.setup = () => {
@@ -20,14 +23,26 @@ const checkboxMirror = p => {
     p.noLoop();
     slider = p.createSlider(0, 255, 127);
     slider.input(p.onThresholdChange);
+
+    for(let y = 0; y < cols; y++) {
+      for(let x = 0; x < rows; x++) {
+        const box = p.createCheckbox();
+        box.parent("my-container");
+        checkboxes.push(box);
+      }
+      const linebreak = p.createSpan('</br>');
+      linebreak.parent("my-container");
+    }
   };
 
   p.draw = () => {
     p.background(255);
     img.loadPixels();
-    for (var y=0; y < img.height; y+= vScale) {
-      for (var x=0; x < img.width; x+= vScale) {
-        const index = y * img.width + x;
+    console.log(img.width * img.height)
+    for (let y = 0; y < cols; y++) {
+      for (let x = 0; x < rows; x++) {
+        const index = (img.width - x + 1 + (y * img.width)) * vScale;
+        const indexCheckbox = y * rows + x;
         const r = img.pixels[index * 4];
         const g = img.pixels[(index * 4) + 1];
         const b = img.pixels[(index * 4) + 2];
@@ -36,13 +51,15 @@ const checkboxMirror = p => {
         const threshold = slider.value();
         if(bright > threshold) {
           p.fill(255);
+          checkboxes[indexCheckbox].checked(false);
         } else {
           p.fill(0);
+          checkboxes[indexCheckbox].checked(true);
         }
 
         p.noStroke();
         p.rectMode(p.CENTER);
-        p.rect(x, y, vScale, vScale);
+        p.rect(x * vScale, y * vScale, vScale, vScale);
       }
     }
   };
