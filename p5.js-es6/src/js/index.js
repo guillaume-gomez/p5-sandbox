ee86fee4c25b49c5aeb08f3a65a352b2a57e2d0d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-22 19:58:39 +0100

    update commands + example

--- p5.js-es6/src/js/index.js
+++ p5.js-es6/src/js/index.js
@@ -1,14 +1,13 @@
 import p5 from 'p5';
 import "p5/lib/addons/p5.dom";
 
-/*import sketch from "./sketch";
+import sketch from "./sketch";
 import pointillism from "./pointillism";
 import gameOfLife from "./gameOfLife";
-*/
 import spirograph from "./spirograph";
 import pixelize from "./pixelize";
 import toThirdDimension from "./toThirdDimension";
 import checkboxMirror from "./checkboxMirror";
 import terrain from "./terrain";
 import cubeWaveBee from "./cubeWaveBee";
-new p5(cubeWaveBee);
+new p5(terrain);
